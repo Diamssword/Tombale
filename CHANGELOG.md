c643ea0b8cc41216f026259653ecab2ee22fcd21
Diamssword <hugo.piechowiak@proton.me>
2026-09-15 11:03:41 +0200

    changed work pipeline

---
+++ CHANGELOG.md
@@ -0,0 +1,1 @@
+Updated manifest to latest version
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,36 +1,17 @@
-import java.util.Properties
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven {
+            name = "AzureDoom Maven"
+            url = uri("https://maven.azuredoom.com/mods")
+        }
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 
 rootProject.name = "Tombale"
-// 1. Load properties from gradle.properties
-val props = Properties()
-file("gradle.properties").inputStream().use { props.load(it) }
-// 2. Access your property
-val modVersion = props.getProperty("modVersion")
-plugins {
-    // See documentation on https://scaffoldit.dev
-    id("dev.scaffoldit") version "0.2.14+"
-}
-
-// Would you like to do a split project?
-// Create a folder named "common", then configure details with `common { }`
-
-hytale {
-    usePatchline("release")
-    useVersion("0.5.0")
-
-    repositories {
-        // Any external repositories besides: MavenLocal, MavenCentral, HytaleMaven, and CurseMaven
-    }
-
-    dependencies {
-        // Any external dependency you also want to include
-    }
-    manifest {
-        Group = "Diamssword"
-        Name = "Tombale"
-        Main = "com.diamssword.tombale.Tombale"
-        Version = modVersion
-        Description = "A grave mod for Hytale!"
-        IncludesAssetPack = true
-    }
-}
+// Gradle automatically imports gradle/libs.versions.toml as the libs catalog.
